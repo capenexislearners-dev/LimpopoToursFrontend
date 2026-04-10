698aa068470d1ecffffd0e399657638e1a40bc48
proliner <proliner@DESKTOP-QSJ92T2>
2026-04-10 12:09:06 +0200

    Added Booking, Guide and Tour Entities inside Models folder

---
+++ LimpopoToursFrontend/Models/Tour.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LimpopoToursFrontend.Models
+{
+    public class Tour
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int DurationDays { get; set; }
+        public string Location { get; set; }
+        public string? TourImageurl { get; set; }
+        public Guid? GuideId { get; set; }
+        public List<Booking> Bookings { get; set; } = new();
+    }
+}
